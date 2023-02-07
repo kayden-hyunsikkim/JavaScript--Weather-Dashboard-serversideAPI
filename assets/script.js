ea0c27883d49682404f1a8f3eb3c5d219d7ba288
hyunsik kim (kayden) <kayden190116@gamil.com>
2023-02-07 12:58:28 +1030

    edited fetching api

--- assets/script.js
+++ assets/script.js
@@ -13,29 +13,47 @@ let count = 0;
 
 
 
-searchBtn.addEventListener('click', getApi);
+searchBtn.addEventListener('click', getGEO);
 
 
-function getApi(event) {
+function getGEO(event) {
     event.preventDefault();
     let Citytosearch = userCity.value;
     if (Citytosearch) {
-        let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + Citytosearch + '&appid=fe5f18ad8da81e94eabca7fc60f10944&units=metric';
+        let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${Citytosearch}&limit=5&appid=fe5f18ad8da81e94eabca7fc60f10944`;
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
-                localStorage.setItem(Citytosearch, JSON.stringify(data));
-                todaysWeather(data);
-                dateforecast(data);
-                showingSearchedcity(data);
+                localStorage.setItem(`${Citytosearch} GEO`, JSON.stringify(data));
+                getApi(data);
             })
+
     } else {
-        userCity.setAttribute("placeholder", "Please enter a city name")
+        userCity.setAttribute("placeholder", "Please enter a city name");
     }
+}
+
+
+function getApi(data) {
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=fe5f18ad8da81e94eabca7fc60f10944&units=metric`;
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        localStorage.setItem(`${userCity.value}`, JSON.stringify(data));
+        todaysWeather(data);
+        dateforecast(data);
+        showingSearchedcity(data);
+    })
 
 }
+
 
 
 function todaysWeather(data, cityweather) {
@@ -58,7 +76,7 @@ function todaysWeather(data, cityweather) {
 
     headingToday.setAttribute("class", "element");
     headingToday.setAttribute("style", "display:inline-block;");
-    headingToday.innerHTML = data.city.name + " " + date;
+    headingToday.innerHTML = userCity.value + " " + date;
 
     img.setAttribute("src", iconurl);
     img.setAttribute("id", "img");
@@ -88,58 +106,65 @@ function todaysWeather(data, cityweather) {
 
 function dateforecast(data) {
 
-    if (count !== 0){
+    if (count !== 0) {
         let section = document.querySelector("#cards");
         section.remove();
     }
 
     let section = document.createElement("section");
-    section.setAttribute("id","cards")
+    section.setAttribute("id", "cards")
     section.setAttribute("class", "d-flex");
-  
+
     days.setAttribute("style", "display:block");
 
-    for(i=0,j=7; i<5; i++,j=j+8){
+    for (i = 0, j = 7; i < 5; i++, j = j + 8) {
 
-    let div = document.createElement("div");
-    div.classList = "card  border border-4 border-dark"
-    div.setAttribute("style","width: 14rem; margin-right: 24px")
-    let ul = document.createElement("ul");
-    ul.setAttribute("class","list-group")
-
-    let dateli1 = document.createElement("li");
-    dateli1.setAttribute("class", "list-group-item");
-    dateli1.setAttribute("style","font-size:1.4em; font-weight:bold;");
-    let dateimg = document.createElement("li");
-    let img = document.createElement("img");
-    dateimg.setAttribute("class", "list-group-item");
-    img.setAttribute("id", "date1img");
-    let datetemp = document.createElement("li");
-    datetemp.classList ="list-group-item textstyle";
-    let datewind = document.createElement("li");
-    datewind.classList ="list-group-item textstyle";
-    let datehumidity = document.createElement("li");
-    datehumidity.classList ="list-group-item textstyle";
-    
+        let div = document.createElement("div");
+        div.classList = "card  border border-4 border-dark";
+        div.setAttribute("style", "width: 14rem; margin-right: 24px;");
 
 
-    let datejs = dayjs(data.list[j].dt_txt).format('D/MMMM/YYYY');
-    let date1conurl = "./assets/icons/" + data.list[j].weather[0].icon + ".png";
-    dateli1.innerHTML = datejs;
-    img.setAttribute("src", date1conurl);
-    datetemp.innerHTML = "Temp: " + data.list[j].main.temp;
-    datewind.innerHTML = "Wind: " + data.list[j].wind.speed + " Mph";
-    datehumidity.innerHTML = "humidity: " + data.list[j].main.humidity;
+        let ul = document.createElement("ul");
+        ul.setAttribute("class", "list-group");
 
-    result.appendChild(section);
-    section.appendChild(div);
-    div.appendChild(ul);
-    ul.append(dateli1,dateimg,datetemp,datewind,datehumidity);
-    dateimg.appendChild(img)
-    
-    count++;
-    
-}
+        let dateli1 = document.createElement("li");
+        dateli1.setAttribute("class", "list-group-item");
+        dateli1.setAttribute("style", "font-size:1.4em; font-weight:bold;");
+
+        let dateimg = document.createElement("li");
+        let img = document.createElement("img");
+        dateimg.setAttribute("class", "list-group-item");
+        img.setAttribute("id", "date1img");
+
+        let datetemp = document.createElement("li");
+        datetemp.classList = "list-group-item textstyle";
+
+        let datewind = document.createElement("li");
+        datewind.classList = "list-group-item textstyle";
+
+        let datehumidity = document.createElement("li");
+        datehumidity.classList = "list-group-item textstyle";
+
+
+
+        let datejs = dayjs(data.list[j].dt_txt).format('DD/MMM/YYYY');
+        let date1conurl = "./assets/icons/" + data.list[j].weather[0].icon + ".png";
+
+        dateli1.innerHTML = datejs;
+        img.setAttribute("src", date1conurl);
+        datetemp.innerHTML = "Temp: " + data.list[j].main.temp;
+        datewind.innerHTML = "Wind: " + data.list[j].wind.speed + " Mph";
+        datehumidity.innerHTML = "humidity: " + data.list[j].main.humidity;
+
+        result.appendChild(section);
+        section.appendChild(div);
+        div.appendChild(ul);
+        ul.append(dateli1, dateimg, datetemp, datewind, datehumidity);
+        dateimg.appendChild(img)
+
+        count++;
+
+    }
 
 }
 
@@ -151,7 +176,7 @@ function showingSearchedcity(data) {
     buttenforsaved.setAttribute("class", "btn btn-primary savedcityBtn");
     buttenforsaved.setAttribute("value", userCity.value);
     buttenforsaved.setAttribute("onclick", 'callingsavedData(event)');
-    buttenforsaved.innerHTML = data.city.name;
+    buttenforsaved.innerHTML = userCity.value;
     form.appendChild(divforsaved);
     divforsaved.appendChild(buttenforsaved);
 
@@ -162,7 +187,6 @@ let callingsavedData = function (event) {
     event.preventDefault();
     let savedcity = event.target.getAttribute('value');
     let savedCityobject = JSON.parse(localStorage.getItem(savedcity));
-    console.log(savedCityobject.list[0]);
     todaysWeather(savedCityobject);
     dateforecast(savedCityobject);
 };
